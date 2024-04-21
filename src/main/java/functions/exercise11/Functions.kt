5c1684b1580b932e4006113394442e065b1c2e15
IlyaGlaz <glazterix@gmail.com>
2024-04-21 09:29:17 +0300

    functions5

---
+++ src/main/java/functions/exercise11/Functions.kt
@@ -0,0 +1,18 @@
+package functions.exercise11
+
+import functions.exercise10.curry
+
+/**
+ * Напишите функцию fun, которая меняет местами аргументы каррированной функции.
+ */
+fun main() {
+    println(curry<Int, Int, Int>{ x, y -> x - y }(10)(20))
+}
+
+// Решение
+fun <T, U, V> swapArgs(f: (T) -> (U) -> V): (U) -> (T) -> V =
+    { a: U ->
+        { b: T -> f(b)(a) }
+    }
+
+
