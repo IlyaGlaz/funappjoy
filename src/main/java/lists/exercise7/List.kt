497623204c646bfc3a8b54cdb2fdd67de6dcca91
IlyaGlaz <glazterix@gmail.com>
2024-09-01 11:46:42 +0300

    Lists 7

--- src/main/java/lists/exercise7/List.kt
+++ src/main/java/lists/exercise7/List.kt
@@ -7,7 +7,7 @@ import lists.exercise7.List.Nil
  * Напишите функцию, вычисляющую произведение элементов списка вещественных чисел, использовав рекурсивный подход.
  */
 fun main() {
-
+    println(sum(List(20, 45)))
 }
 
 //Решение
@@ -31,6 +31,9 @@ sealed class List<out A> {
     fun concat(list: List<@UnsafeVariance A>): List<A> = concat(this, list)
 
     fun reverse(): List<A> = reverse(invoke(), this)
+
+    fun <B> foldRight(identity: B, f: (A) -> (B) -> B): B =
+        foldRight(this, identity, f)
 
     object Nil : List<Nothing>() {
         override fun init(): List<Nothing> = throw IllegalArgumentException()
@@ -77,12 +80,46 @@ sealed class List<out A> {
                 is Cons -> reverse(acc.cons(list.head), list.tail)
             }
 
+        fun <A, B> foldRight(
+            list: List<A>,
+            identity: B,
+            f: (A) -> (B) -> B
+        ): B =
+            when (list) {
+                Nil -> identity
+                is Cons -> f(list.head)(foldRight(list.tail, identity, f))
+            }
+
         operator fun <A> invoke(vararg az: A): List<A> =
             az.foldRight(Nil as List<A>) { a: A, list: List<A> -> Cons(a, list) }
     }
 }
 
-fun sum(list: List<Int>): Int = when (list) {
-    Nil -> 0
-    is Cons -> sum(list.tail) + list.head
-}
+// Не абстрагированные версии
+//fun sum(list: List<Int>): Int = when (list) {
+//    Nil -> 0
+//    is Cons -> sum(list.tail) + list.head
+//}
+//
+//fun product(list: List<Double>): Double = when (list) {
+//    Nil -> 0.0
+//    is Cons -> if (list.head == 0.0)
+//        0.0
+//    else
+//        list.head * product(list.tail)
+//}
+
+// Абстрагированние этих функций в одну функцию
+fun <A, B> foldRight(
+    list: List<A>,
+    identity: B,
+    f: (A) -> (B) -> B
+): B =
+    when (list) {
+        Nil -> identity
+        is Cons -> f(list.head)(foldRight(list.tail, identity, f))
+    }
+
+fun sum(list: List<Int>): Int = foldRight(list, 0) { x -> { y -> x + y } }
+
+fun product(list: List<Double>): Double = foldRight(list, 1.0) { x -> { y -> x * y } }
